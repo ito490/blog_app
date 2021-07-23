f07ed86067541f4345eb402695b6382e4d688a96
ito490 <ito.ss490@gmail.com>
2021-07-23 14:37:46 +0900

    fakerインストール・show作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
     def index
         # render 'home/index'
-        @article = Article.first
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 end
